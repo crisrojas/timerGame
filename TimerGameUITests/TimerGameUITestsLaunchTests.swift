4babc64d0eff92d375cd0e4c2cc3596118958d1d
Cristian <cristian.rojas@live.fr>
2022-04-16 23:53:44 +0200

    Initial Commit

---
+++ TimerGameUITests/TimerGameUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TimerGameUITestsLaunchTests.swift
+//  TimerGameUITests
+//
+//  Created by Cristian Rojas on 16/04/2022.
+//
+
+import XCTest
+
+class TimerGameUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
